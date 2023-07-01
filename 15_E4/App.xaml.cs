d6491b24b10248b7bd9ece81680b5a87b6fa0f7f
XxFJxX <ferinocente357@gmail.com>
2023-06-30 23:59:49 -0600

    Agregar archivos de proyecto.

---
+++ 15_E4/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace _15_E4;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ 15_E4/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace _15_E4;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+
+		double Ga = Convert.ToDouble(G.Text);
+        double Pr = Convert.ToDouble(P.Text);
+
+        double Li = Ga * 3.78541;
+		double To = Li * Pr;
+
+		la.Text = "Total a pagar : $" + To;
+
+    }
+}
+
